fb6f83afa4393ea22cf31c4f5aa87172923e984c
Shawn Catanzarite <me@shawncatz.com>
2024-07-10 14:07:53 +0000

    add nats error handler to detect slow consumption

--- mercury.go
+++ mercury.go
@@ -1,6 +1,8 @@
 package mercury
 
 import (
+	"fmt"
+
 	"github.com/nats-io/nats.go"
 )
 
@@ -23,7 +25,7 @@ type Message struct {
 func New(name string, URL string) (*Mercury, error) {
 	m := &Mercury{Name: name, URL: URL}
 
-	nc, err := nats.Connect(URL)
+	nc, err := nats.Connect(URL, nats.ErrorHandler(m.errorHandler))
 	if err != nil {
 		return nil, err
 	}
@@ -37,6 +39,20 @@ func New(name string, URL string) (*Mercury, error) {
 	m.nc = nc
 	m.ec = c
 	return m, nil
+}
+
+func (m *Mercury) errorHandler(_ *nats.Conn, sub *nats.Subscription, natsErr error) {
+	fmt.Printf("error: %v\n", natsErr)
+	if natsErr == nats.ErrSlowConsumer {
+		pendingMsgs, _, err := sub.Pending()
+		if err != nil {
+			fmt.Printf("couldn't get pending messages: %v", err)
+			return
+		}
+		fmt.Printf("Falling behind with %d pending messages on subject %q.\n", pendingMsgs, sub.Subject)
+	} else {
+		fmt.Printf("nats error on %q: %s\n", sub.Subject, natsErr)
+	}
 }
 
 func (m *Mercury) Status() nats.Status {
